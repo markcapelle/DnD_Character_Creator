44f86a13b57857e38dd5700c95613494feea6ed6
Mark Van Capelle <markcapelle@outlook.ie>
2026-03-31 13:44:59 +0100

    Test: created base html files and tested rendering from flask.

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# app code
+
+
+# pages
 @app.route("/")
-def indext():
+def index():
     return render_template("index.html")
+
+@app.route("/your_character")
+def your_character():
+    return render_template("your_character.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/tutorial")
+def tutorial():
+    return render_template("tutorial.html")
+
+@app.route("/advice")
+def advice():
+    return render_template("advice.html")
 
 if __name__ == "__main__":
     app.run()
