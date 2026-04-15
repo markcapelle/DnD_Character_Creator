df838fe40935fa323d86d79b63d50cf1df351372
Mark Van Capelle <markcapelle@outlook.ie>
2026-04-15 21:38:53 +0100

    Style: add favicon.

--- app.py
+++ app.py
@@ -698,5 +698,13 @@ def reset():
     session.clear()  # wipes ALL session data
     return {"success": True}
 
+
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+
 if __name__ == "__main__":
     app.run()
