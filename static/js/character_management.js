51ed87254979fda993b8f6494aa832374a486aca
Mark Van Capelle <markcapelle@outlook.ie>
2026-04-16 18:48:26 +0100

    Style: CSS for index.html

--- static/js/character_management.js
+++ static/js/character_management.js
@@ -3,8 +3,7 @@ function changeHP(direction) {
     fetch(`/hp/${direction}`, { method: "POST" })
         .then(res => res.json())
         .then(data => {
-            document.getElementById("hp-display").textContent =
-                `${data.current_hp} / ${data.max_hp}`;
+            document.getElementById("hp-display").textContent = `${data.current_hp} / ${data.max_hp}`;
         });
 }
 
